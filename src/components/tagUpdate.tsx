07c35ad9661aed2dde924ce5212bde6f5359d38a
Matias <matiaszuniga93@gmail.com>
2024-05-25 23:48:33 -0600

    add styles

--- src/components/tagUpdate.tsx
+++ src/components/tagUpdate.tsx
@@ -1,51 +1,58 @@
-'use client'
-import React, { useEffect } from 'react'
+"use client";
+import React from "react";
 import useTagInput from "@/hooks/useTag";
 import { TagField } from "./tagField";
 import { Button } from "@/components/ui/button";
-import { redirect } from 'next/navigation'
 
-const TagUpdate = ({params,id}:any) => {
-    const { tags, handleAddTag, handleRemoveTag } = useTagInput(5, params);
+const TagUpdate = ({ params, id }: any) => {
+  const { tags, handleAddTag, handleRemoveTag } = useTagInput(5, params);
 
+  const handleSend = async () => {
+    try {
+      const data = {
+        id: parseInt(id),
+        tags: tags,
+      };
+      const response = await fetch(`http://localhost:3000/api/detail`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-    const handleSend = async () => {
-      try {
-        const data = {
-          id: parseInt(id),
-          tags:tags,
-        };
-        const response = await fetch(`http://localhost:3000/api/detail`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-
-        if (!response.ok) {
-          throw new Error("Hubo un problema al enviar los datos.");
-        }
-        window.location.reload();
-      } catch (error) {
-        console.error("Error al enviar los datos a la API:", error);
+      if (!response.ok) {
+        throw new Error("Hubo un problema al enviar los datos.");
       }
-    };
+      window.location.reload();
+    } catch (error) {
+      console.error("Error al enviar los datos a la API:", error);
+    }
+  };
 
   return (
-    <form className='flex flex-col h-[250px] justify-between items-center' onSubmit={(e) => e.preventDefault()}>
-    <TagField
-      tags={tags}
-      addTag={handleAddTag}
-      removeTag={handleRemoveTag}
-      maxTags={5}
-    />
-    {
-      JSON.stringify(params) !== JSON.stringify(tags) ? 
-      <Button className={`m-4 mr-10 mb-8 bg-orange-400 hover:bg-orange-500`} onClick={handleSend}>guardar</Button> : ""
-    }
-  </form>
-  )
-}
+    <form
+      className="flex flex-col h-[250px] justify-between items-center"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <TagField
+        tags={tags}
+        addTag={handleAddTag}
+        removeTag={handleRemoveTag}
+        maxTags={5}
+      />
+      {JSON.stringify(params) !== JSON.stringify(tags) ? (
+        <Button
+          className={`m-4 mr-10 mb-8 bg-orange-400 hover:bg-orange-500`}
+          onClick={handleSend}
+        >
+          guardar
+        </Button>
+      ) : (
+        ""
+      )}
+    </form>
+  );
+};
 
-export default TagUpdate
+export default TagUpdate;
